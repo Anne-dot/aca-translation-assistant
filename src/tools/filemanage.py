84613b6ce184fa9fb893cac68cd73579f6d92c99
thersgo <90255869+thersgo@users.noreply.github.com>
2026-09-18 16:41:55 +0300

    added type hinting to `tools/filemanage.py`

--- src/tools/filemanage.py
+++ src/tools/filemanage.py
@@ -1,28 +1,24 @@
-"""
-Functions for working with direcories and files.
-"""
-
 import json
 import csv
 from pathlib import Path
 
 
-def ensure_directory_exists(file_path):
+def ensure_directory_exists(file_path: str) -> None:
     Path(file_path).parent.mkdir(parents=True, exist_ok=True)
 
 
-def read_csv_file(file_path, delimiter="\t"):
+def read_csv_file(file_path: str, delimiter: str = "\t") -> str:
     with open(file_path, "r", encoding="utf-8") as f:
         reader = csv.reader(f, delimiter=delimiter)
         for row in reader:
             yield row
 
 
-def load_json_file(file_path):
+def load_json_file(file_path: str) -> any:
     with open(file_path, "r", encoding="utf-8") as f:
         return json.load(f)
 
 
-def save_json_file(data, file_path, indent=2):
+def save_json_file(data: any, file_path: str, indent: int = 2) -> None:
     with open(file_path, "w", encoding="utf-8") as f:
         json.dump(data, f, ensure_ascii=False, indent=indent)
